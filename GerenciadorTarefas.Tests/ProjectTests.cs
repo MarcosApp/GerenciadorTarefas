ee1d98e1a85b0643601b459aebc15e9f976ae676
Marcos Gotado <masgotado@apoioprodesp.sp.gov.br>
2024-01-11 16:21:54 -0300

    Adicionando Task e Project teste

---
+++ GerenciadorTarefas.Tests/ProjectTests.cs
@@ -0,0 +1,66 @@
+﻿using Domain.Contracts.UseCases.AddProject;
+using Domain.Entities;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Controllers;
+using WebAPI.Models.Project;
+using Xunit;
+
+namespace GerenciadorTarefas.Tests
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void ListProject_ReturnsOk_WhenProjectsExist()
+        {
+            // Arrange
+            var projectUseCaseMock = new Mock<IProjectUseCase>();
+            projectUseCaseMock.Setup(pc => pc.ListProject()).Returns(new List<Project>());
+
+            var controller = new ProjectsController(
+                projectUseCaseMock.Object,
+                Mock.Of<IValidator<AddProjectInput>>()
+            );
+
+            // Act
+            var result = controller.ListProject();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.NotNull(okResult);
+
+            var projects = okResult.Value as List<Project>;
+            Assert.NotNull(projects);
+        }
+
+        [Fact]
+        public void ListProject_ReturnsOkWithMessage_WhenNoProjectsExist()
+        {
+            // Arrange
+            var projectUseCaseMock = new Mock<IProjectUseCase>();
+            projectUseCaseMock.Setup(pc => pc.ListProject()).Returns(new List<Project>());
+
+            var controller = new ProjectsController(
+                projectUseCaseMock.Object,
+                Mock.Of<IValidator<AddProjectInput>>()
+            );
+
+            // Act
+            var result = controller.ListProject();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.NotNull(okResult);
+
+            Assert.Equal("Nenhum projeto criado.", okResult.Value);
+        }
+    }
+}
---
+++ GerenciadorTarefas.Tests/TaskTests.cs
@@ -0,0 +1,70 @@
+﻿using Domain.Contracts.UseCases.AddProject;
+using Domain.Contracts.UseCases.AddTask;
+using Domain.Contracts.UseCases.AddUser;
+using Domain.Entities;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using WebAPI.Controllers;
+using WebAPI.Models.Task;
+using Xunit;
+
+namespace GerenciadorTarefas.Tests
+{
+    public class TaskTests
+    {
+        [Fact]
+        public void ListTask_ReturnsOk_WhenTasksExist()
+        {
+            // Arrange
+            var taskUseCaseMock = new Mock<ITaskUseCase>();
+            taskUseCaseMock.Setup(tc => tc.ListTask()).Returns(new List<Task>());
+
+            var controller = new TaskController(
+                taskUseCaseMock.Object,
+                Mock.Of<IProjectUseCase>(),
+                Mock.Of<IUserUseCase>(),
+                Mock.Of<IValidator<AddTaskInput>>(),
+                Mock.Of<IValidator<UpdateTaskInput>>()
+            );
+
+            // Act
+            var result = controller.ListTask();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.Equal("Nenhuma task criado.", okResult.Value);
+
+            var tasks = okResult.Value as List<Task>;
+            Assert.Null(tasks);
+        }
+
+        [Fact]
+        public void ListTask_ReturnsOkWithMessage_WhenNoTasksExist()
+        {
+            // Arrange
+            var taskUseCaseMock = new Mock<ITaskUseCase>();
+            taskUseCaseMock.Setup(tc => tc.ListTask()).Returns(new List<Task>());
+
+            var controller = new TaskController(
+                taskUseCaseMock.Object,
+                Mock.Of<IProjectUseCase>(),
+                Mock.Of<IUserUseCase>(),
+                Mock.Of<IValidator<AddTaskInput>>(),
+                Mock.Of<IValidator<UpdateTaskInput>>()
+            );
+
+            // Act
+            var result = controller.ListTask();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.NotNull(okResult);
+
+            Assert.Equal("Nenhuma task criado.", okResult.Value);
+        }
+    }
+}
